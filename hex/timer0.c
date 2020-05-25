4d3822400bbe623f5c3279d9af63cff0a312c9d7
hjoaquim <h.joaquim@campus.fct.unl.pt>
2020-05-25 18:28:17 +0100

    Revert "interrupt"
    This reverts commit 32ab46d4068c5e6e05d7a4be85ef70e29bfaa02c.

--- hex/timer0.c
+++ hex/timer0.c
@@ -10,35 +10,10 @@
 void timer0_init() {
     TMR0=0;	//TMR0 interrupt is generated when the TMR0 register overflows from FFh to 00h
     
-    T0CS=0; 	// bit 5 -> 0 = Internal instruction cycle clock
-    T0SE=0; 	// bit 4 -> 0 = Increment on low-to-high transition on T0CKI pin 
-    PSA=0;		// bit 3 -> 0 = Prescaler is assigned to the Timer0 module 
-    
-	//bits 0-2
-	/// 111 -> TMR0 Rate= 1:256
-	///		-> WDT Rate = 1:128
-	PS0=1;
+    T0CS=0;
+    T0SE=0; 
+    PSA=0;
+    PS0=1;
     PS1=1;
     PS2=1;
-	
-	TMR0IF = 0; 
-	// The flag is set on Overflow
-	// Bit TMR0IF must be cleared in software by the Timer0
-	// module Interrupt Service Routine before re-enabling
-	// this interrupt.
 }
-
-//counter
-void timer1_init() {
-	
-    TMR1CS = 1;		//bit 1 -> 0 = Internal clock (FOSC/4)
-    
-					//timer1 input clock presclae select bits
-					// 00 -> 1:1 prescale value
-	T1CKPS0 = 0;
-    T1CKPS1 = 0;
-    
-    TMR1 = 0; 		
-    
-    TMR1ON = 1;
-}    
--- hex/timer0.h
+++ hex/timer0.h
@@ -2,6 +2,5 @@
 #define	TIMER0_H
 
 	void timer0_init();
-	void timer1_init();
 
 #endif	/* TIMER0_H */
